b1d358348d0a7ae4e4bfdd005fb215a4a2d96114
Khao Seekakun <71384546+Khaoseekakun@users.noreply.github.com>
2023-01-17 19:51:53 +0700

    ไฟล์คลิป EP4

---
+++ EP4/DiscordBot/index.js
@@ -0,0 +1,24 @@
+const {Client, GatewayIntentBits, Collection} = require("discord.js");
+
+require("dotenv").config()
+
+const client = new Client({
+    intents:[
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+module.exports = client;
+
+client.login(process.env.token);
+
+// Collection
+client.messageCommands = new Collection();
+client.aliase = new Collection();
+client.slashCommands = new Collection();
+client.events = new Collection();
+
+require("./handler")(client);
